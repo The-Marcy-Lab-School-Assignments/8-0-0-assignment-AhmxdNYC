16683261f58e13977f2df10c4b5f2c1b1f27924f
benspector-mls <ben@marcylabschool.org>
2024-04-09 15:19:51 -0400

    code graded: 12/13 tests passing

--- server/index.js
+++ server/index.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+
+/* FEEDBACK: Use arrow functions! */
+
+/* FEEDBACK: nice middleware!! */
 function logger(req, res, next) {
   console.log(`${req.method} ${req.url}`);
   next();
 }
+
 function sendHtmlResponse(req, res, next) {
   res.send("<h1>Hello, World!</h1>");
 }
 function sendDataResponse(req, res, next) {
   res.json({ message: "Hello, World!" });
 }
+
+/* FEEDBACK: I don't think you have an about.html file?? */
 function sendHtmlFile(req, res, next) {
   res.sendFile(__dirname + "/about.html");
 }
+
+/* FEEDBACK: Very nice! */
 function sendDataWithQueryParam(req, res, next) {
   const name = req.query.name || "World";
   res.json({ message: `Hello, ${name}!` });
 }
+
 app.get("/", logger, sendHtmlResponse);
 app.get("/about", logger, sendHtmlFile);
 app.get("/api/data", logger, sendDataResponse);
 app.get("/api/greeting", logger, sendDataWithQueryParam);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
